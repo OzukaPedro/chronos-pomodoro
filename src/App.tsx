ba06eeee97f2b7dab5bc670c1f31a942e42a64ad
Pedro Campagnoli <pedrocampagnoli28@gmail.com>
2025-05-27 14:33:45 -0300

    começo context e providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Home } from "./pages/Home";
+import { TaskContextProvider } from "./contexts/TaskContext";
+
 import "./styles/globals.css";
 import "./styles/theme.css";
 
 function App() {
   return (
-    <>
+    <TaskContextProvider>
       <Home />
-    </>
+    </TaskContextProvider>
   );
 }
 
